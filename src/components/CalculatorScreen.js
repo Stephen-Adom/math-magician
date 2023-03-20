b3f5adbc0314ce708b5ec00592983829dbdbf4ed
Stephen Adom Addae <saaddae003@st.ug.edu.gh>
2023-03-20 13:46:49 +0000

    create calculator component for screen and number pad

---
+++ src/components/CalculatorScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function CalculatorScreen() {
+  return (
+    <div className="calculator-screen">0</div>
+  );
+}
+
+export default CalculatorScreen;
---
+++ src/components/ComponentBody.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function ComponentBody() {
+  return (
+    <div className="calcuator-body">
+      <div className="number-pad">
+        AC
+      </div>
+      <div className="number-pad">
+        +/-
+      </div>
+      <div className="number-pad">
+        %
+      </div>
+      <div className="number-pad">
+        -
+      </div>
+      <div className="number-pad">
+        7
+      </div>
+      <div className="number-pad">
+        8
+      </div>
+      <div className="number-pad">
+        9
+      </div>
+      <div className="number-pad">
+        X
+      </div>
+    </div>
+  );
+}
+
+export default ComponentBody;
