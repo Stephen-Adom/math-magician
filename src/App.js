ece6bf137786749fefb0e14a49fd8f477e587f30
Stephen Adom Addae <saaddae003@st.ug.edu.gh>
2023-03-20 11:07:38 +0000

    add script for running linters for jsx

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
-          {' '}
           <code>src/App.js</code>
-          {' '}
           and save to reload.
         </p>
         <a
