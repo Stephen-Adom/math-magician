434002f1410e0e6037dab070d8eb8e86306c24f5
Stephen Adom Addae <saaddae003@st.ug.edu.gh>
2023-03-22 09:21:30 +0000

    add fetch quote service

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
-import Calculator from './components/Calculator';
+import Calculator from './components/calculator/Calculator';
+import Quotes from './components/quotes/Quotes';
 
 export const resultContext = React.createContext('0');
 
 function App() {
   return (
     <div className="App">
-      <Calculator />
+      <div className="row h-100 m-0">
+        <div className="col-lg-6 col-md-6 col-sm-12 col-12">
+          <Calculator />
+        </div>
+        <div className="col-lg-6 col-md-6 col-sm-12 col-12">
+          <Quotes />
+        </div>
+      </div>
     </div>
   );
 }
