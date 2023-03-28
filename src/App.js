68512baf75389fddd1b82f0eba0c2f3abf1ce27c
Stephen Adom Addae <saaddae003@st.ug.edu.gh>
2023-03-28 12:30:29 +0000

    design home page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Navigation />
-      <div className="container py-5">
+      <div className="container py-5" style={{ height: '80vh' }}>
         <Outlet />
       </div>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,32 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Home = () => (
-  <div id="Home-section">
-    <h2 className="fw-bold">Welcome to our page</h2>
-    <article className="fs-5">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
-      debitis, eius cumque, at voluptatem id ex enim ipsam beatae quam obcaecati
-      asperiores explicabo, fugiat atque! Eum corrupti itaque porro? Enim
-      obcaecati quia labore reiciendis magni consequuntur provident maiores nisi
-      tenetur mollitia voluptatibus ratione pariatur cupiditate beatae
-      inventore, sunt, ex, excepturi possimus consequatur ipsa aut porro
-      officia? Voluptas
-    </article>
+const Home = () => {
+  const navigate = useNavigate();
 
-    <article className="fs-5">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum dolore
-      facilis ratione porro, officia placeat repudiandae vero tempore facere
-      quae dolorum voluptate possimus quos sequi incidunt assumenda molestias.
-      Odio nesciunt voluptatibus maxime autem nihil, delectus quaerat
-      necessitatibus possimus id voluptates enim velit aspernatur error expedita
-      neque recusandae odit dolores accusantium illo ea cupiditate voluptatem,
-      quae nulla! Quis obcaecati nemo veniam, molestias voluptatum maxime amet
-      eaque deleniti consequatur dolorem minima vel ex omnis error eius officiis
-      numquam, laboriosam cumque qui dolorum! Id, illo cumque eaque est laborum
-      ex recusandae ullam nihil natus nisi voluptas quaerat qui quos alias
-      praesentium eveniet blanditiis.
-    </article>
-  </div>
-);
+  return (
+    <div id="Home-section">
+      <h1 className="fw-bold display-6">Welcome to Math Magician</h1>
+      <article className="fs-5 mt-5">
+        <h2>The best way to learn Mathematics is to do Mathematics!!</h2>
+        <p className="w-50">Start learning mathematics with our free leassons and discover new career paths</p>
+        <div className="d-flex align-items-center gap-3">
+          <button type="button" className="btn btn-primary btn-lg" onClick={() => navigate('calculator')}>Get Started</button>
+          <button type="button" className="btn btn-outline-primary btn-lg" onClick={() => navigate('quote')}>Get Motivated with Quotes</button>
+        </div>
+      </article>
+
+      <article className="fs-5 mt-4">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum dolore
+        facilis ratione porro, officia placeat repudiandae vero tempore facere
+        quae dolorum voluptate possimus quos sequi incidunt assumenda molestias.
+        Odio nesciunt voluptatibus maxime autem nihil, delectus quaerat
+        necessitatibus possimus id voluptates enim velit aspernatur error expedita
+        neque recusandae odit dolores accusantium illo ea cupiditate voluptatem,
+        quae.
+      </article>
+    </div>
+  );
+};
 
 export default Home;
