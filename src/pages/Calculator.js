279f672c418f169fd52a7eb42c9cb92d7c35ccbf
Stephen Adom Addae <saaddae003@st.ug.edu.gh>
2023-03-28 12:11:09 +0000

    complete the design for calculator page

--- src/pages/Calculator.js
+++ src/pages/Calculator.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { resultContext } from '../App';
 import Teacher from '../assets/Teacher-PNG-Picture.png';
+import SpeakDialog from '../assets/dialogue-window-148176_640.webp';
 
 import CalculatorScreen from '../components/calculator/CalculatorScreen';
 import ComponentBody from '../components/calculator/ComponentBody';
@@ -10,8 +11,10 @@ function Calculator() {
 
   return (
     <section className="row calculator-page">
-      <div className="col-lg-6 col-md-12 col-sm-12 col-12">
+      <div className="col-lg-6 col-md-12 col-sm-12 col-12 position-relative">
         <img src={Teacher} width="100%" alt="teacher" />
+        <img src={SpeakDialog} width="100%" className="dialog-image" alt="dialog" />
+        <h5 className="welcome-label">{'Let\'s do some math!'}</h5>
       </div>
       <div className="col-lg-6 col-md-12 col-sm-12 col-12">
         <section className="container-fluid">
